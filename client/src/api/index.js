0318257020769f41812c5b766714ec68926f9e4d
Matthew Theodore Borromeo <105698549+Smile-369@users.noreply.github.com>
2023-08-03 15:29:21 +0800

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
 	// baseURL: 'https://restaurantchecker-api.vercel.app/api',
 	// baseURL: 'http://localhost:8080/api',
-	baseURL: 'https://ccapdev-phase3-group5-kb53.vercel.app/',
+	baseURL: 'https://ccapdev-phase3-group5-kb53-fl8r8zzrc-smile-369.vercel.app/',
 	headers: { 'Content-Type': 'application/json' },
 	withCredentials: true,
 });
